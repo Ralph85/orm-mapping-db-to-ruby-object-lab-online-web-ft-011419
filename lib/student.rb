4cff5c9de0adcb4900a5ac245e9b6bce792ee941
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-04-28 03:54:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,9 +32,9 @@ end
     DB[:conn].execute(sql)
   end
 
-  def self.first_students_in_grade_10
+  def self.first_X_students_in_grade_10(x)
     sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql, x)
   end
 
   def self.first_student_in_grade_10
