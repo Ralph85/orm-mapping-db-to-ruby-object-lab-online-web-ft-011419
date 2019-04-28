6d05feafd9e9f4fc03b049dd4ecad63b2a28c0ab
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-04-28 03:50:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,7 +27,7 @@ end
     DB[:conn].execute(sql)
   end
 
-  def self.students_below_grade_12
+  def self.all_students_below_grade_12
     sql = "SELECT * FROM students WHERE grade < 12"
     DB[:conn].execute(sql)
   end
